f756a23ef04a231c061c9c72ab543950116c762c
Felix <mahammadabdulazizov@outlook.com>
2025-05-09 22:51:03 +0200

    small updates to button size, look

--- To_Do/gui.py
+++ To_Do/gui.py
@@ -9,7 +9,8 @@ sg.theme("Dark")
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=50, image_source="add.png", mouseover_colors="#D2B48C",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
